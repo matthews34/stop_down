8a6d9eff3e9e7e87429ff75d7da465cc6b91d663
Matheus Vieira <matheus.sousav@gmail.com>
2019-04-16 22:21:37 -0300

    Add files for database testing

--- api/app.py
+++ api/app.py
@@ -1,13 +1,36 @@
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://stopdown:semtempoirmao@localhost/stopdown'
+db = SQLAlchemy(app)
 api = Api(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+_user = api.model('User', {
+    'id': fields.Integer,
+    'username': fields.String,
+    'email': fields.String
+})
 
 @api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+@api.route('/users')
+class UserController(Resource):
+    @api.marshal_with(_user)
+    def get(self):
+        return User.query.all()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
