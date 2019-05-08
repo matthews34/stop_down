68c0dcb7a445de7f73c1ba40330032c46fc32b8e
Matheus Vieira <matheus.sousav@gmail.com>
2019-05-08 09:35:06 -0300

    Add Funcionario entity in the db
    Add Funcionario entity with 'id', 'name', 'email' and 'password'
    attributes

---
+++ api/app/main/model/funcionario.py
@@ -0,0 +1,17 @@
+"""
+Descrição deste arquivo: aqui é criado o modelo de Funcionario, que é apenas uma
+classe herdeira do objeto db.Model
+"""
+
+from app.main import db
+
+class Funcionario(db.Model):
+    __tablename__ = 'funcionarios'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(32), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Funcionario %r>' % self.name
