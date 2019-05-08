14b5a48e657e714167a939218ca71fb4e807f14c
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-05-08 11:28:01 -0300

    Replace 'cpf' to 'CPF' to standardize

--- api/app/main/service/funcionario_service.py
+++ api/app/main/service/funcionario_service.py
@@ -6,20 +6,25 @@ usados pelas rotas do controller
 from app.main import db
 from app.main.model.funcionario import Funcionario
 
+
 def save_new_funcionario(data):
     funcionario = Funcionario(
         nome=data['nome'],
+        CPF=data['CPF'],
         email=data['email'],
         senha=data['senha']
-        )
+    )
     save_changes(funcionario)
+
 
 def save_changes(data):
     db.session.add(data)
     db.session.commit()
 
+
 def get_all_funcionarios():
     return Funcionario.query.all()
+
 
 def get_a_funcionario(id):
     return Funcionario.query.filter_by(id=id).first()
