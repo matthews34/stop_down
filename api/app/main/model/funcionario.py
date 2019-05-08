7b2c3159b0c0686eeeeead2e7a11533bc17ead4b
Matheus Vieira <matheus.sousav@gmail.com>
2019-05-08 09:44:27 -0300

    Add CPF to funcionario

--- api/app/main/model/funcionario.py
+++ api/app/main/model/funcionario.py
@@ -9,6 +9,7 @@ class Funcionario(db.Model):
     __tablename__ = 'funcionarios'
     
     id = db.Column(db.Integer, primary_key=True)
+    CPF = db.Column(db.Integer, unique=True, nullable=False)
     name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(32), unique=False, nullable=False)
